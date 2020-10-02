01b0373d7fd18849b67292106e95dd9a3b381e50
Roman Bitetti <roman.bitetti@tutanota.com>
2020-10-02 19:49:39 +0200

    Libft 42 Sao Paulo Oct-2020

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: robitett <robitett@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/02 18:42:45 by robitett          #+#    #+#             */
-/*   Updated: 2020/10/02 19:42:02 by robitett         ###   ########.fr       */
+/*   Updated: 2020/10/02 19:49:28 by robitett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list *new_lst;
 	t_list *tmp;
 
+	new_lst = NULL;
 	while (lst)
 	{
 		tmp = ft_lstnew(f((*lst).content));
