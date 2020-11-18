68cfd704b856c82de2ddb80dcdefd9a5be2b40b7
Roman Bitetti <roman.bitetti@tutanota.com>
2020-11-17 22:03:12 -0500

    %p

--- ft_printf/format_pointer.c
+++ ft_printf/format_pointer.c
@@ -6,7 +6,7 @@
 /*   By: romanbtt <marvin@student.42sp.org.br>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/17 11:39:12 by romanbtt          #+#    #+#             */
-/*   Updated: 2020/11/17 21:15:30 by romanbtt         ###   ########.fr       */
+/*   Updated: 2020/11/17 22:01:00 by romanbtt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,15 @@
 void format_pointer(t_flags *x)
 {
 	char *str;
-	char *test;
 	int i;
 
 	i = 0;
-	test = ullitoa_base(va_arg(x->args, unsigned long long int), 16);
-	str = ft_strjoin("0x", test);
+	str = ullitoa_base(va_arg(x->args, unsigned long long int), 16);
 	while (str[i++])
 		str[i] = ft_tolower(str[i]);
 	if (!x->minus)
 		padding(x->width, ft_strlen(str), x);
+	print("0x", 2, x);
 	print(str, ft_strlen(str), x);
 	if (x->minus)
 		padding(x->width, ft_strlen(str), x);
